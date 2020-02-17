20e73f31eed6353f23ba42d50a965ae8a3e08fa0
stella-celentano <stellacelentano99@gmail.com>
2020-02-16 23:16:18 -0300

    serie service modified

--- src/app/series/serie.service.ts
+++ src/app/series/serie.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Serie } from './serie.model';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SerieService {
 
-  constructor() { }
+  private url = 'https://localhost:3000/serie';
+
+  constructor(private http: HttpClient) { }
+
+  getSeries(): Observable<Serie[]> {
+    return this.http.get<Serie[]>(this.url);
+  }
 }
