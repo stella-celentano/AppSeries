49cdf1c2f3c317518c735fdc55d71199c598302f
Stella Celentano <stellacelentano99@gmail.com>
2020-02-17 15:54:39 -0300

    service e api, estabelencendo ligação para o back

--- src/app/series/serie.service.ts
+++ src/app/series/serie.service.ts
@@ -2,17 +2,13 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Serie } from './serie.model';
 import { HttpClient} from '@angular/common/http';
+import { api } from './../app.api';
 
-@Injectable({
-  providedIn: 'root'
-})
 export class SerieService {
-
-  private url = 'http://localhost:3000';
 
   constructor(private http: HttpClient) { }
 
   getSeries(): Observable<Serie[]> {
-    return this.http.get<Serie[]>(this.url);
+    return this.http.get<Serie[]>(`${api}/serie`);
   }
 }
