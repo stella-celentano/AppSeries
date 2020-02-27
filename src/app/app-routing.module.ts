ce0d368b73811fc39ef8ac0a5171490b34e58b9a
Stella Celentano <stellacelentano99@gmail.com>
2020-02-27 00:35:22 -0300

    Route create serie component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SeriesComponent } from './series/series.component';
+import { CreateSerieComponent } from './series/create-serie/create-serie.component'
 
 
 export const ROUTES: Routes = [
-  {path: 'series', component: SeriesComponent}
+  {path: 'series', component: SeriesComponent},
+  {path: 'series/create', component: CreateSerieComponent}
 ];
 
 @NgModule({
